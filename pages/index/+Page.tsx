2cfdfa8769317da53ab63855248b7b07095b6abd
Ivan Uzun <ivan@motorway.co.uk>
2024-09-16 00:34:14 +0100

    fix: scope transition updates

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -1,18 +1,28 @@
 import { useUnit } from 'effector-react';
 import React from 'react';
 
+import { appService } from '@services/app';
+
 import { Counter } from './Counter.js';
 import { model } from './model';
 import { RandomButton } from './RandomButton';
 
 export default function Page() {
-  const [data, random, telefunc] = useUnit([model.$data, model.$random, model.$telefunc]);
+  const [data, random, telefunc, appState, appCounter, incCounterFx] = useUnit([
+    model.$data,
+    model.$random,
+    model.$telefunc,
+    appService.$appState,
+    appService.$appCounter,
+    appService.incCounterFx,
+  ]);
   return (
     <>
       <h1>My Vike app</h1>
       This page is:
       <ul>
         <li>Rendered to HTML.</li>
+        <li suppressHydrationWarning>App State: {appState}</li>
         <li>
           Initiated on client. <Counter />
         </li>
@@ -20,6 +30,9 @@ export default function Page() {
         <li>From server: {random}</li>
         <li>
           Telefunc: {telefunc} <RandomButton />
+        </li>
+        <li>
+          Async Counter: {appCounter}. <button onClick={incCounterFx}>inc</button>
         </li>
       </ul>
     </>
