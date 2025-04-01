1bf68ce4908649e833a4d5741864694f843d8c00
Ivan Uzun <ivan@motorway.co.uk>
2025-04-01 11:54:19 +0100

    chore: eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,33 @@
 // @ts-nocheck
 
-import eslint from "@eslint/js";
+import eslint from '@eslint/js';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
-import prettier from "eslint-plugin-prettier/recommended";
-import react from "eslint-plugin-react/configs/recommended.js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import prettier from 'eslint-plugin-prettier/recommended';
+import react from 'eslint-plugin-react/configs/recommended.js';
+import reactJsxRuntime from 'eslint-plugin-react/configs/jsx-runtime.js';
+import globals from 'globals';
+import tsEslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default tsEslint.config(
   {
     ignores: [
-      "dist/*",
+      'dist/*',
       // Temporary compiled files
-      "**/*.ts.build-*.mjs",
+      '**/*.ts.build-*.mjs',
 
       // JS files at the root of the project
-      "*.js",
-      "*.cjs",
-      "*.mjs",
+      '*.js',
+      '*.cjs',
+      '*.mjs',
     ],
   },
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tsEslint.configs.recommended,
   {
     languageOptions: {
       parserOptions: {
-        sourceType: "module",
-        ecmaVersion: "latest",
+        sourceType: 'module',
+        ecmaVersion: 'latest',
       },
     },
   },
@@ -65,31 +66,35 @@ export default tseslint.config(
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/consistent-type-imports': 'error',
       // other
-      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-      "@typescript-eslint/no-unused-vars": [
+      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+      '@typescript-eslint/no-unused-vars': [
         1,
         {
-          argsIgnorePattern: "^_",
+          argsIgnorePattern: '^_',
         },
       ],
+    },
+    languageOptions: {
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+      },
     },
   },
 
   {
-    files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
+    files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
     ...react,
+    ...reactJsxRuntime,
+
     languageOptions: {
       ...react.languageOptions,
-      globals: {
-        ...globals.serviceworker,
-        ...globals.browser,
-      },
     },
 
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
   },
